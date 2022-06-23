c6963d6b5bedaf29791b7856da1ca207c86f330c
victor8001 <victor.tsai@tpisoftware.com>
2022-06-23 17:06:40 +0800

    init commit

---
+++ src/main/java/idv/victor/app/SpringDistributedSessionApplication.java
@@ -0,0 +1,13 @@
+package idv.victor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDistributedSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDistributedSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/victor/app/SpringDistributedSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.victor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDistributedSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
